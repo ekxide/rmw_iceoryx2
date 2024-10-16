2caedae370d0602a11193529a04e4ca789614de3
Jeff Ithier <jeff@ithier.dev>
2024-10-16 14:28:38 +0200

    Mark RMW Event as unsupported

--- rmw_iceoryx2_cxx/src/rmw/event.cpp
+++ rmw_iceoryx2_cxx/src/rmw/event.cpp
@@ -15,24 +15,24 @@ extern "C" {
 
 rmw_ret_t
 rmw_publisher_event_init(rmw_event_t* rmw_event, const rmw_publisher_t* publisher, rmw_event_type_t event_type) {
-    IOX_TODO();
+    return RMW_RET_UNSUPPORTED;
 }
 
 rmw_ret_t rmw_subscription_event_init(rmw_event_t* rmw_event,
                                       const rmw_subscription_t* subscription,
                                       rmw_event_type_t event_type) {
-    IOX_TODO();
+    return RMW_RET_UNSUPPORTED;
 }
 
 rmw_ret_t rmw_take_event(const rmw_event_t* event_handle, void* event_info, bool* taken) {
-    IOX_TODO();
+    return RMW_RET_UNSUPPORTED;
 }
 
 rmw_ret_t rmw_event_fini(rmw_event_t* event) {
-    IOX_TODO();
+    return RMW_RET_UNSUPPORTED;
 }
 
 rmw_ret_t rmw_event_set_callback(rmw_event_t* event, rmw_event_callback_t callback, const void* user_data) {
-    IOX_TODO();
+    return RMW_RET_UNSUPPORTED;
 }
 }
