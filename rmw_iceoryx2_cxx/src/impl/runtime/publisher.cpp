4c7f13ddffe50d54a59e875a2521e34f53b97522
Jeff Ithier <jeff@ithier.dev>
2025-02-12 14:49:09 +0100

    [#2] Dynamically increase publisher payload size as needed

--- rmw_iceoryx2_cxx/src/impl/runtime/publisher.cpp
+++ rmw_iceoryx2_cxx/src/impl/runtime/publisher.cpp
@@ -56,6 +56,7 @@ Publisher::Publisher(CreationLock,
     auto publisher = iox2_pubsub_service.value()
                          .publisher_builder()
                          .initial_max_slice_len(::rmw::iox2::message_size(m_typesupport))
+                         .allocation_strategy(::iox2::AllocationStrategy::PowerOfTwo)
                          .create();
     if (!publisher.has_value()) {
         RMW_IOX2_CHAIN_ERROR_MSG(::iox2::bb::into<const char*>(publisher.error()));
